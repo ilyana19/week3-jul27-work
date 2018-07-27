bd8f6f0ee6ef1a5bc940e38e01824d2367c66ed7
ilyana19 <aries_2152@hotmail.com>
2018-07-26 21:34:19 -0400

    finished task list exercise

--- task.rb
+++ task.rb
@@ -1,17 +1,17 @@
 class Task
+  attr_accessor :description, :due_date
+
   def initialize(description, due_date)
     @description = description
     @due_date = due_date
   end
-
-  attr_accessor :description, :due_date
 end
 
-task1 = Task.new("do ruby hw", "Jul29")
-task2 = Task.new("hw2", "Jul28")
-task3 = Task.new("hw3", "Jul31")
-
-p task1.description # => "do ruby hw"
-p task2.due_date # => "Jul28"
-p task2.due_date = "Aug1" # => "Aug1"
-p task3.due_date # => "Jul31"
+# task1 = Task.new("do ruby hw", "Jul29")
+# task2 = Task.new("hw2", "Jul28")
+# task3 = Task.new("hw3", "Jul31")
+#
+# p task1.description # => "do ruby hw"
+# p task2.due_date # => "Jul28"
+# p task2.due_date = "Aug1" # => "Aug1"
+# p task3.due_date # => "Jul31"
--- todolist.rb
+++ todolist.rb
@@ -1,3 +1,27 @@
-class TodoList
-  
+require_relative 'task'
+
+class TodoList < Task
+  def initialize
+    @tasks = []
+  end
+
+  def add_task(task)
+    @tasks << task
+  end
 end
+
+task1 = Task.new("homework1", "2018/07/27")
+task2 = Task.new("homework2", "2018/07/27")
+task3 = Task.new("homework3", "2018/07/27")
+
+list = TodoList.new
+list.add_task(task1)
+list.add_task(task2)
+list.add_task(task3)
+
+p list
+
+task3.due_date = "2018/07/28"
+
+puts ""
+p list
