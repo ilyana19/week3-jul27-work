1bc0303867991ddbb5fcd928e11436d7af4eb0e7
ilyana19 <aries_2152@hotmail.com>
2018-07-26 21:22:02 -0400

    started todo list exercise

---
+++ task.rb
@@ -0,0 +1,17 @@
+class Task
+  def initialize(description, due_date)
+    @description = description
+    @due_date = due_date
+  end
+
+  attr_accessor :description, :due_date
+end
+
+task1 = Task.new("do ruby hw", "Jul29")
+task2 = Task.new("hw2", "Jul28")
+task3 = Task.new("hw3", "Jul31")
+
+p task1.description # => "do ruby hw"
+p task2.due_date # => "Jul28"
+p task2.due_date = "Aug1" # => "Aug1"
+p task3.due_date # => "Jul31"
